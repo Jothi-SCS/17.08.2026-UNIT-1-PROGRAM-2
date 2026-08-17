60fddddfb502900f09c7f12c17922a4409635287
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-17 13:11:37 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -10,3 +10,14 @@
 -- DOB            - DATE, NOT NULL
 -- Gender         - VARCHAR(10), NOT NULL
 -- DepartmentID   - INT(5), NOT NULL
+CREATE DATABASE IF NOT EXISTS CollegeDB;
+
+USE CollegeDB;
+
+CREATE TABLE Student (
+    StudentID INT(5) NOT NULL PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
