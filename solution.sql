95620351b9fa099a322d827cf9a51bab1cd4b91a
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-17 13:00:53 +0530

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,12 @@
+-- Student Table Assignment
+-- Student Name:
+-- Register Number:
+
+-- Write your SQL program below.
+
+-- Requirements:
+-- StudentID      - INT(5), PRIMARY KEY, NOT NULL
+-- StudentName    - VARCHAR(20), UNIQUE, NOT NULL
+-- DOB            - DATE, NOT NULL
+-- Gender         - VARCHAR(10), NOT NULL
+-- DepartmentID   - INT(5), NOT NULL
